374dfcc08aca95185907ce8a69ca277a424961ac
Jacob Lee <hyunjacoblee@gmail.com>
2016-03-06 09:33:52 -0500

    Update rede.php

--- rede.php
+++ rede.php
@@ -2,12 +2,16 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
+    <meta name="description" content="A lightweight autocomplete plugin for jQuery">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1" user-scalable=no>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../favicon.ico">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300">
+    <link rel="stylesheet" href="css/jquery.auto-complete.css">
 
     <title>Rede</title>
 
@@ -17,12 +21,13 @@
     <!-- Custom styles for this template -->
     <link href="css/main.css" rel="stylesheet">
     <link href="css/additional.css" rel="stylesheet">
-    <link href="css/daterangepicker.css" rel="stylesheet">
 
     <!--fonts-->
     <link async href='https://fonts.googleapis.com/css?family=Avenir:100,800|Lato' rel='stylesheet' type='text/css'>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
-    <script src="http://ajax.aspnetcdn.com/ajax/jquery.validate/1.14.0/jquery.validate.min.js"></script>
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+    <script src="//code.jquery.com/jquery-1.10.2.js"></script>
+    <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
     <script>
       $(function(){
             if($(window).width() < 1025){
@@ -30,6 +35,25 @@
             $('nav').removeClass('navbar-static-top').addClass('navbar-fixed-top');
             $('body').css({"padding-top": "70px"});
             }
+
+        var availableTags = [
+          "Diabetes",
+          "High Blood Pressure",
+          "Dehydration",
+          "",
+        ];
+        var availableTags1 = [
+          "Kidney Problems",
+          "Vision Problems",
+          "Foot Numbness",
+        ];
+        $('#advise').tokenfield({
+          autocomplete: {
+            source: ['red','blue','green','yellow','violet','brown','purple','black','white'],
+            delay: 100
+          },
+          showAutocompleteOnFocus: true
+        })
         });
     </script>
   </head>
@@ -68,15 +92,34 @@
 
     <?php 
 
-     $link = mysqli_connect("localhost","root","", "rede") or die("failed to connect to server!!");
+     $link = mysqli_connect("","root","", "rede") or die("failed to connect to server!!");
 
-     $sql = 'SELECT * FROM patient'; 
+     $sql = 'SELECT * FROM Sam'; 
 
      $record = mysqli_query($link, $sql);
+
+    if(isset($_POST['send'])){
+    $errorMessage = "";
+    $name=$_POST['name'];
+    $email=$_POST['email'];
+    $hometown=$_POST['hometown'];
+    $daterange=$_POST['daterange'];
+
+    echo "<p><span class='glyphicon glyphicon-ok-sign'></span>Great! You'll receive a confirmation email in a moment.</p>"."\n";
+
+    $insqDbtb="INSERT INTO `Sam`
+    (`causes`, `advise`) VALUES ('$causes', '$advise')";
+    mysqli_query($link,$insqDbtb) or die(mysqli_error($link));
+   }
+
+      mysqli_close($link);
     ?>
 
     <div class="site-wrapper">
-        <div class="inner cover">
+
+      <div class="inner-cover">
+          <div class="col-sm-6 blog-sidebar">
+          <div class="sidebar-module sidebar-module-inset">
               <table class="table table-striped">
                 <thead>
                   <tr>
@@ -84,56 +127,64 @@
                     <th>Value</th>
                     <th>Normal</th>    
                     <th>New</th>
-                    <th>Important</th>
-                    <th>Meaning</th>
-                    <th>To Do</th>
+                    <th>Education Level</th>
                   </tr>
                 </thead>
                 <tbody>
                   <?php
                    while($patient=mysqli_fetch_assoc($record)){
-                    echo "<tr>";
+                    if($patient['biomarker_value']<=$patient['biomarker_low']||$patient['biomarker_value']>=$patient['biomarker_high']){
+                    echo "<tr class='danger'>";
+                  } else if ($patient['biomarker_value']>$patient['biomarker_low']||$patient['biomarker_value']<$patient['biomarker_high']){
+                    echo "<tr class='success'>";
+                  }
 
-                    echo "<td>".$patient['biomarker']."</td>";
-                    echo "<td>".$patient['biomarker_value']."</td>";
-                    echo "<td>".$patient['biomarker_range']."</td>";
-
-                    echo "<td>"."<input type='text' name='name'>"."</td>";
-                    echo "<td>"."<input type='text' name='name'>"."</td>";
-                    echo "<td>"."<input type='text' name='name'>"."</td>";
-                    echo "<td>"."<input type='text' name='name'>"."</td>";
+                    echo "<td align='left'>".$patient['biomarker']."</td>";
+                    echo "<td align='left'>".$patient['biomarker_value']."</td>";
+                    echo "<td align='left'>".$patient['biomarker_range']."</td>";
+                    echo "<td align='left'>"."<label class='radio-inline'>"."<input type='radio' name='cf' id='optionsRadios1' value='1'>"."Yes"."</label>"."<label class='radio-inline'>"."<input type='radio' name='cf' id='optionsRadios2' value='2'>"."No"."</label>"."</td>";
+                    echo "<td align='left'>"."<div class='controls'>"."<select name='party'>"."<option value='1'>"."Low"."</option>"."<option value='2'>"."Medium"."</option>"."<option value='3'>"."High"."</option>"."</td>";
 
                     echo "</tr>";
                    }
                   ?>
                 </tbody>
               </table>
-
-              <div class="control-group" style="text-align: center; padding-top: 10px; padding-bottom: 10px"> 
+          </div>
+          </div> <!--side bar-->v
+          <div class="col-sm-6 blog-sidebar">
+          <form class="form-vertical" style="text-align:left"  id="user_form" role="form" method="post">
+            
+            <div class="control-group col-sm-12">
+              <label class="control-label" for="inputRisks">Causes:</label>
                 <div class="controls">
-                  <button type="submit" name="submit" class="btn">Submit</button>
+                  <textarea class="form-control" id="availableTags" name="risks" type="text" rows="3"></textarea>
                 </div>
-              </div>
-        </div>
-    </div>
+            </div>
 
-    <div class="mastfoot">
-      <div class="inner">
-        <ul>
-          <li><a href="https://twitter.com/AskJacobNYC" target="_blank"><div class="socialmedia" id="twitter"></div></a></li>
-          <li><a href="https://www.instagram.com/askjacob_nyc" target="_blank"><div class="socialmedia" id="instagram"></div></a></li>
-        </ul>
-      </div>
-    </div>
+            <div class="control-group col-sm-12">
+              <label class="control-label" for="inputAdvise">Advise:</label>
+                <div class="controls">
+                  <textarea class="form-control" id="availableTags1" name="advise" type="text" rows="3"></textarea>
+                </div>
+            </div>
+
+           <div class="control-group col-sm-12" style="text-align: center; padding-top: 10px; padding-bottom: 10px"> 
+              <div class="controls">
+                <button type="submit" name="send" class="btn">Send</button>
+              </div>
+           </div>
+          </form>
+        </div>
+     </div>   
+   </div>
+
 
     <!-- Bootstrap core JavaScript
     ================================================== -->
     <!-- Placed at the end of the document so the pages load faster -->
     <script src="js/bootstrap.min.js"></script>
-    <script src="js/moment.min.js"></script>
-
-    <script type="text/javascript">
-  
-    </script>
+    <script src="js/moment.min.js"></script> 
+    <script src="bootstrap-tagautocomplete.js"></script>
   </body>
 </html>
